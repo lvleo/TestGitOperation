6d86434e11f5ac792938d2806816ff75c1584576
leolv <lvyongxu@gmail.com>
2017-08-09 13:48:53 +0800

    add UserModel class & show user name

--- app/src/main/java/com/useeinfo/testgitoperation/MainActivity.java
+++ app/src/main/java/com/useeinfo/testgitoperation/MainActivity.java
@@ -3,6 +3,7 @@ package com.useeinfo.testgitoperation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
 
         Log.e(TAG, "onCreate: 11111111");
+
+
+        UserModel userModel = new UserModel();
+        userModel.setName("leo");
+
+        TextView tv = null;
+        tv.setText(userModel.getName());
+
     }
 
 }
---
+++ app/src/main/java/com/useeinfo/testgitoperation/UserModel.java
@@ -0,0 +1,21 @@
+package com.useeinfo.testgitoperation;
+
+/**
+ * Created by lyx on 2017/8/9 13:45.
+ * Contact:     lvyongxu@gmail.com
+ * Description:
+ */
+
+public class UserModel {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
