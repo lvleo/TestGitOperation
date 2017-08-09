7b94ced04d058d0df234ef9174ed9d3d7b878ee6
leolv <lvyongxu@gmail.com>
2017-08-09 14:21:37 +0800

    add TestModel

---
+++ app/src/main/java/com/useeinfo/testgitoperation/TestModel.java
@@ -0,0 +1,13 @@
+package com.useeinfo.testgitoperation;
+
+/**
+ * Created by lyx on 2017/8/9 14:20.
+ * Contact:     lvyongxu@gmail.com
+ * Description:
+ */
+
+public class TestModel {
+
+    private String address;
+
+}
